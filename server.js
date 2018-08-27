ef093f11e6f98ba1f19e2497b22e0011f90b123e
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-08-27 17:43:09 -0500

    as

--- server.js
+++ server.js
@@ -15,6 +15,71 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(cookieParser());
 server.use(express.static(path.join(__dirname, 'frontend/build')));
 
+
+server.get("/API/login/:userData", (req, res) => {
+    let encoded = req.params.userData;
+    let decoded = base64.decode(encoded);
+    let params = utf8.decode(decoded).split(";;;");
+
+    let connection = mysql.createConnection(process.env.DB_URL);
+    connection.connect();
+    connection.query('SELECT * FROM USERS WHERE EMAIL=\'' + params[0] + '\';', (err, rows, fields) => {
+        if (err) {
+            console.log(err);
+            throw new Error("User not found");
+        }
+        let user = null;
+        if (rows[0].PASSWORD === params[1]) {
+            user = {
+                id: rows[0].ID,
+                name: rows[0].NAME,
+                email: params[0]
+            };
+            console.log("login success");
+        }
+        res.send(user);
+    });
+    connection.end();
+});
+
+server.get("/API/signup/:userData", (req, res) => {
+    let encoded = req.params.userData;
+    let decoded = base64.decode(encoded);
+    let params = utf8.decode(decoded).split(";;;");
+
+    let connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection.connect();
+    try {
+        connection.query('INSERT INTO USERS (NAME, LASTNAME, EMAIL, PASSWORD) ' +
+            'VALUES (\'' + params[0] + '\',\'' + params[1] + '\',\'' + params[2] + '\',\''+params[3]+'\');', (err, rows, fields) => {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+        });
+        connection.query('SELECT ID, NAME FROM USERS WHERE EMAIL=\'' + params[1] + '\';', (err, rows, fields) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            let user = {
+                id: rows[0].ID,
+                name: rows[0].NAME,
+                email: params[1]
+            };
+
+            res.send(user);
+        });
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    } finally {
+        connection.end();
+    }
+
+});
+
 server.listen(process.env.PORT || 3001, () => {
     console.log("Listening...");
 });
